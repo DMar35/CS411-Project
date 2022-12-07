341c21b69afadd60bd28efe8ca2d106c37847a48
Kristi Li <kli1226@bu.edu>
2022-12-07 16:59:52 -0500

    Added some text on Home

--- my-react-app/src/components/NavBar.js
+++ my-react-app/src/components/NavBar.js
@@ -1,7 +1,10 @@
 import "./styles.css"
 import React from "react";
-import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+// import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+// import Container from 'react-bootstrap/Container';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () => {
     return (
@@ -10,19 +13,30 @@ const NavBar = () => {
         //         <Navbar.Brand href="#">Navbar</Navbar.Brand>
         //     </Container>
         // </Navbar>
-        <Navbar expand="lg">
-            <Navbar.Brand href="/">Tutorial</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Form className="form-center">
-            <FormControl type="text" placeholder="Search" className="" />
-        </Form>
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
-                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-            </Nav>
-        </Navbar.Collapse>
-        </Navbar>
+
+        // <Navbar expand="lg">
+        //     <Navbar.Brand href="/">Tutorial</Navbar.Brand>
+        // <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        // <Form className="form-center">
+        //     <FormControl type="text" placeholder="Search" className="" />
+        // </Form>
+        // <Navbar.Collapse id="basic-navbar-nav">
+        //     <Nav className="ml-auto">
+        //         <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
+        //     </Nav>
+        // </Navbar.Collapse>
+        // </Navbar>
+
+    //     <>
+    //   <Navbar bg="light">
+    //     <Container>
+    //       <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+    //     </Container>
+    //   </Navbar>
+    //   </>
+        <div className="navbar">
+            <span className="main">Welcome Back</span>
+        </div>
     )
 }
 export default NavBar
