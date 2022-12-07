83c2350d0d3cbbebed11b8eacfe01d8b8fa3327e
Alx-W <a396700974@gmail.com>
2022-12-07 17:51:04 -0500

    search box

--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -3,13 +3,22 @@ import NavBar from './NavBar';
 import "./styles.css"
 
 const Home = () => {
+const [query, setQuery] = React.useState('');
     return (
         <div className="auth-form-container">
             <p>This is a paragraph and I am writing on the home page</p>
             <p>This is another paragraph, hi hey hello whatsup yo</p>
             <span className="main">Welcome Back</span>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet tempor pulvinar at arcu libero sit id dignissim sollicitudin.</p>
-            
+            <input placeholder="Enter Event to Search" onChange={event => setQuery(event.target.value)} />
+        {
+            // Data.map((post, index) => {
+            // <div key={index}>
+            //     <p>{post.title}</p>
+            //     <p>{post.author}</p>
+            // </div>
+            // })
+        }
         </div>
     )
 }
