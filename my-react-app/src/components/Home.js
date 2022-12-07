8e04b2a09b63c22e52dac2fc7bf11504542868ff
Kristi Li <kli1226@bu.edu>
2022-12-07 17:08:32 -0500

    Added background color for home

--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
 import "./styles.css"
+import {Helmet} from 'react-helmet';
+
 
 const Home = () => {
     return (
         <div className="auth-form-container">
+            <Helmet>
+                <style>{'body { background-color: #A5D5A7; }'}</style>
+            </Helmet>
             <span className="main">Welcome Back</span>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet tempor pulvinar at arcu libero sit id dignissim sollicitudin.</p>
             
